7f864179ab4a24827e52c364dc566b118545f56a
Chris Brown <chris.brown@skookum.com>
2018-12-04 14:21:43 -0700

    🔩

--- 2018/utils/index.js
+++ 2018/utils/index.js
@@ -2,12 +2,11 @@ const compose = (...funs) => (init) => funs.reduceRight((x, f) => f(x), init);
 const head = (arr) => arr[0];
 const log = (label) => (x) => { console.log(`${label}:`, x); return x; };
 const map = (fun) => (arr) => arr.map(fun);
-const reduce = (fun, init = 0) => (arr) => arr.reduce(fun, init);
+const reduce = (fun, init) => (arr) => arr.reduce(fun, init);
 const tail = (arr) => arr.slice(1);
 const countBy = (arr) => arr.reduce((acc, x) => {
-  if (acc[x]) acc[x] += 1;
-  else acc[x] = 1;
-  return x;
+  acc[x] = (acc[x] || 0) + 1;
+  return acc;
 }, {});
 
 module.exports = {
