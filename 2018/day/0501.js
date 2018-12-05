2a2b4d29adc9993ac834d295d127ea49fc3a9aa6
Chris Brown <chris.brown@skookum.com>
2018-12-05 10:33:27 -0700

    ☕️
    changed `getAnswer` again to make sure that the last thing it does is
    call itself. it's definitely tail-call optimized. i thought that node
    supported TCO, at least with a --harmony flag or something. it's
    supported in ES6 but not implemented by node?

--- 2018/day/0501.js
+++ 2018/day/0501.js
@@ -11,23 +11,25 @@ const areOppositePolarity = (chr1, chr2) =>
 
 const answer = 'dabCBAcaDA'
 
-const getAnswer = (list, result, reaction) => {
-  const completePassWithNoReaction = (list.length === 1 && !reaction)
-  if (completePassWithNoReaction)
-    return result + head(list)
+const getAnswer = (list) => {
+  const _getAnswer = (_list, _result, _reaction) => {
+    // complete pass and no reaction = DONE
+    return (_list.length === 1 && !_reaction)
+      ? _result + head(_list)
+      // complete pass with reaction = start over
+      : (_list.length === 1 && _reaction)
+      ? _getAnswer(_result + head(_list), '', false)
+      // R-R-R-REACTION!
+      : (areOppositePolarity(head(_list), head(tail(_list))))
+      ? _getAnswer(tail(tail(_list)), _result, true)
+      // mundane
+      : _getAnswer(tail(_list), _result + head(_list), _reaction)
+  }
 
-  const completePassWithReaction = (list.length === 1 && reaction)
-  if (completePassWithReaction)
-    return getAnswer(result + head(list), '', false)
-
-  const isReaction = (areOppositePolarity(head(list), head(tail(list))))
-  if (isReaction)
-    return getAnswer(tail(tail(list)), result, true)
-
-  return getAnswer(tail(list), result + head(list), reaction)
+  return _getAnswer(list, '', false)
 }
 
-const main = (str) => getAnswer(str, '', false)
+const main = (str) => getAnswer(str)
 
 module.exports = {
   main,
